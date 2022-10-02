3a79feece90bf31acf52f27cb0f8944081579674
Hung Tran <tmhung88@outlook.com>
2022-10-02 12:20:17 +0700

    Make app port configurable

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -2,8 +2,9 @@ export interface AppConfig {
   port: number;
 }
 
+const defaultPort = 5000;
 export const appConfigFactory = (): { app: AppConfig } => ({
   app: {
-    port: 5000,
+    port: parseInt(process.env.APP_PORT) ?? defaultPort,
   },
 });
