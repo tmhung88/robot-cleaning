f6dad9a30d3af43fe12a449630b0220b34f3bb0b
Hung Tran <tmhung88@outlook.com>
2022-10-02 12:39:15 +0700

    Remove the configurable port

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -2,9 +2,8 @@ export interface AppConfig {
   port: number;
 }
 
-const defaultPort = 5000;
 export const appConfigFactory = (): { app: AppConfig } => ({
   app: {
-    port: parseInt(process.env.APP_PORT) ?? defaultPort,
+    port: 5000,
   },
 });
