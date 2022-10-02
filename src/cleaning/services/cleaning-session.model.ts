d74fb9a1415b92c77c54b0b2a3d4817c49dc5752
Hung Tran <tmhung88@outlook.com>
2022-10-02 18:47:03 +0700

    Add some comments

--- src/cleaning/services/cleaning-session.model.ts
+++ src/cleaning/services/cleaning-session.model.ts
@@ -3,6 +3,34 @@ import { CleanExecution } from 'src/cleaning/clean-execution.entity';
 import { assertNonNullable } from 'src/helper/assert.helper';
 import { PerformanceHelper } from 'src/helper/performance.helper';
 
+export class CleaningSession {
+  static readonly MIN_X = -100000;
+  static readonly MAX_X = 100000;
+  static readonly MIN_Y = -100000;
+  static readonly MAX_Y = 100000;
+  static readonly MAX_STEPS = 100000 - 1;
+  static readonly MAX_COMMANDS = 10000;
+
+  static execute(input: CleaningInput): Pick<CleanExecution, 'commands' | 'duration' | 'uniqueVisits'> {
+    const [uniqueVisits, duration] = PerformanceHelper.measureDuration(() => {
+      const cleaningRobot = new CleaningRobot(input.start);
+      const visitTracker = new VisitTracker();
+      visitTracker.trackPositions([input.start]);
+      input.commands.forEach((command) => {
+        const visits = cleaningRobot.move(command);
+        visitTracker.trackPositions(visits);
+      });
+
+      return visitTracker.countUniqueVisits();
+    });
+    return { commands: input.commands.length, uniqueVisits, duration };
+  }
+}
+
+/**
+ * Handle moving in a grid.
+ * When a robot hits a boundary, then the robot cannot move further
+ */
 class CleaningRobot {
   constructor(private position: Position) {}
 
@@ -59,9 +87,23 @@ class CleaningRobot {
   }
 }
 
-class Bucket {
-  public static BUCKET_SIDE = 64;
-  private static MAX_CAPACITY = Bucket.BUCKET_SIDE * Bucket.BUCKET_SIDE;
+type PositionHash = string;
+class Partition {
+  /**
+   * A partition's capacity should be small so that the partition can be quickly full
+   * and its memory is released.
+   *
+   * However, the capacity must not be too small because a large grid can end up
+   * with an excessive number of partitions. The max partitions are 2^24 which is
+   * the default number of keys a JS Map can hold.
+   */
+  public static PARTITION_SIDE = 64;
+
+  private static MAX_VISIT_CAPACITY = Partition.PARTITION_SIDE * Partition.PARTITION_SIDE;
+
+  /**
+   * trackedPositions is null when the partition is full
+   */
   private trackedPositions: Set<PositionHash> | null = new Set<PositionHash>();
   private isFull = false;
 
@@ -71,7 +113,7 @@ class Bucket {
     }
     assertNonNullable(this.trackedPositions, 'trackedPositions must not be null when bucket is not full');
     this.trackedPositions.add(this.hashPosition(position));
-    if (this.trackedPositions.size === Bucket.MAX_CAPACITY) {
+    if (this.trackedPositions.size === Partition.MAX_VISIT_CAPACITY) {
       this.isFull = true;
       this.trackedPositions = null;
     }
@@ -79,7 +121,7 @@ class Bucket {
 
   countUniqueVisits(): number {
     if (this.isFull) {
-      return Bucket.MAX_CAPACITY;
+      return Partition.MAX_VISIT_CAPACITY;
     }
 
     assertNonNullable(this.trackedPositions, 'trackedPositions must not be null when bucket is not full');
@@ -87,66 +129,48 @@ class Bucket {
   }
 
   private hashPosition({ x, y }: Position): PositionHash {
-    return `${x % Bucket.BUCKET_SIDE}:${y % Bucket.BUCKET_SIDE}`;
+    return `${x % Partition.PARTITION_SIDE}:${y % Partition.PARTITION_SIDE}`;
   }
 }
 
+type PartitionHash = string;
+
+/**
+ * Track unique visits of a cleaning session.
+ *
+ * To avoid out of memory, a grid is split into square partitions.
+ * When all positions inside a partition are visited, the partition is considered as "full",
+ * and the memory of tracked visits are released.
+ */
 class VisitTracker {
-  private bucketMap = new Map<string, Bucket>();
+  private partitionMap = new Map<PartitionHash, Partition>();
 
   trackPositions(positions: Position[]): void {
     positions.forEach((position) => this.trackPosition(position));
   }
 
   countUniqueVisits(): number {
-    return Array.from(this.bucketMap.values()).reduce(
+    return Array.from(this.partitionMap.values()).reduce(
       (prevCount, currentPartition) => prevCount + currentPartition.countUniqueVisits(),
       0,
     );
   }
 
   private trackPosition(position: Position): void {
-    const bucket = this.resolveBucket(position);
-    bucket.track(position);
+    const partition = this.resolvePartition(position);
+    partition.track(position);
   }
 
-  private resolveBucket(position: Position): Bucket {
-    const bucketHash = `${Math.floor(position.x / Bucket.BUCKET_SIDE)}:${Math.floor(position.y / Bucket.BUCKET_SIDE)}`;
-    const bucket = this.bucketMap.get(bucketHash);
-    if (bucket != null) {
-      return bucket;
+  private resolvePartition(position: Position): Partition {
+    const partitionHash = `${Math.floor(position.x / Partition.PARTITION_SIDE)}:${Math.floor(
+      position.y / Partition.PARTITION_SIDE,
+    )}`;
+    const partition = this.partitionMap.get(partitionHash);
+    if (partition != null) {
+      return partition;
     }
-    const newBucket = new Bucket();
-    this.bucketMap.set(bucketHash, newBucket);
+    const newBucket = new Partition();
+    this.partitionMap.set(partitionHash, newBucket);
     return newBucket;
-  }
-}
-
-type PositionHash = string;
-
-export class CleaningSession {
-  static readonly MIN_X = -100000;
-  static readonly MAX_X = 100000;
-  static readonly MIN_Y = -100000;
-  static readonly MAX_Y = 100000;
-  static readonly MAX_STEPS = 100000 - 1;
-  static readonly MAX_COMMANDS = 10000;
-
-  static execute(input: CleaningInput): Pick<CleanExecution, 'commands' | 'duration' | 'uniqueVisits'> {
-    const [uniqueVisits, duration] = PerformanceHelper.measureDuration(() => {
-      const cleaningRobot = new CleaningRobot(input.start);
-      const visitTracker = new VisitTracker();
-      visitTracker.trackPositions([input.start]);
-      input.commands.forEach((command, index) => {
-        if (index % 100 === 0) {
-          console.log(`command ${index}`);
-        }
-        const visits = cleaningRobot.move(command);
-        visitTracker.trackPositions(visits);
-      });
-
-      return visitTracker.countUniqueVisits();
-    });
-    return { commands: input.commands.length, uniqueVisits, duration };
   }
 }
--- src/helper/performance.helper.ts
+++ src/helper/performance.helper.ts
@@ -1,4 +1,8 @@
 export class PerformanceHelper {
+  /**
+   * Measure the duration of a function call. The duration is in seconds
+   * but also capture nano milliseconds.
+   */
   static measureDuration<T>(fn: () => T): [T, number] {
     const startTime = process.hrtime();
     const result = fn();
